336e2e05da240f694259fd322066244dd6edf768
Rafael Pazos <rarod@microsoft.com>
2022-06-14 10:57:42 +0200

    fix tf

--- cluster_deployment/providers.tf
+++ cluster_deployment/providers.tf
@@ -1,16 +1,17 @@
 terraform {
+  required_version = "~> 1.1.0"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "= 3.0.0"
+      version = "~> 3.0.0"
+    }
+    local = {
+      source  = "hashicorp/local"
+      version = "~> 2.1.0"
     }
   }
 }
 
 provider "azurerm" {
   features {}
-  subscription_id   = "95b44dd6-5808-485e-9f1a-923eaeef3b37"
-  tenant_id         = "72f988bf-86f1-41af-91ab-2d7cd011db47"
-  client_id         = "67194970-b255-4ab1-b6b7-678b907b8651"
-  client_secret     = "JOZ8Q~d5x42NBSG2wMSVYPMhPc2japnDIoI-rb.P"
 }
